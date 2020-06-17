93580f4d51b2646bfe200cdab2a9d0918fb5cfc5
Daron <daronspence@gmail.com>
2020-06-17 14:53:20 -0500

    styling

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom'
 import Box from './Components/Box'
 import RainingTacos from './Components/RainingTacos'
 
-
 const App = () => {
   const [isShowingWinners, setIsShowingWinners] = useState(false)
   const [isPlayingVideo, setIsPlayingVideo] = useState(false)
@@ -21,14 +20,31 @@ const App = () => {
             {!isShowingWinners && (
               <>
                 {isPlayingVideo ? (
-                  <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/npjF032TDDQ?autoplay=1"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
+                  <div
+                    style={{
+                      height: 0,
+                      paddingBottom: '56.25%',
+                      position: 'relative',
+                      minWidth: 320,
+                      width: '100%',
+                    }}
+                  >
+                    <iframe
+                      width="100%"
+                      height="315"
+                      src="https://www.youtube.com/embed/npjF032TDDQ?autoplay=1"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      style={{
+                        position: 'absolute',
+                        top: '0',
+                        left: '0',
+                        width: '100%',
+                        height: '100%',
+                      }}
+                    ></iframe>
+                  </div>
                 ) : (
                   <div className="flex flex-col items-center">
                     <button
@@ -44,9 +60,18 @@ const App = () => {
                 )}
               </>
             )}
-            {isPlayingVideo && <div>
-                <h1 className="mt-8 text-4xl font-bold">Happy Birthday John!</h1>
-            </div>}
+            {isPlayingVideo && (
+              <div className="p-4 relative z-50">
+                <h1 className="mt-8 text-4xl font-bold leading-tight">
+                  Happy Birthday John!
+                </h1>
+                <p className="mt-2">
+                  Thanks for all your hard work and friendship to folks around
+                  the world 🌍 💜
+                </p>
+                <p className="mt-1">- Daron</p>
+              </div>
+            )}
           </Box>
         </Box>
       </Box>
